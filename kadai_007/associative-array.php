6158fcd06eee77eb17b39dfe6f5441650b21d881
rie1105 <rie1105@piano.ocn.ne.jp>
2024-08-15 06:50:47 +0900

    7章課題アップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+
+<body>
+     <?php
+    // 連想配列に値を代入する
+    $yasai_data = ['name' => 'onion' , 'price' => '200' , 'weight' => '160'];
+
+    // 連想配列の値を出力する
+    print_r($yasai_data);
+     ?>
+</body>
+
+</html>
