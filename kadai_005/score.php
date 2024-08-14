d6185e0bf742b688ceb86f3295f653f42f1c540f
rie1105 <rie1105@piano.ocn.ne.jp>
2024-08-14 10:46:25 +0900

    5章課題アップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 変数に値を代入する
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        // 変数の合計点を算出して変数に代入する
+        $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+        // 変数の平均点を算出して出力する
+
+        echo $sum/10;
+        ?>
+    </p>
+   
+</body>
+
+</html>
